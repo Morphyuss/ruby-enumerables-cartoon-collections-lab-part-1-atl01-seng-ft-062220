3767a350e765e8e3edc8b2f91ecae895b10c46cc
James Stamps Jr <jstampshere@gmail.com>
2020-04-08 23:09:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,14 @@
 def greet_characters(array)
+  greet_characters.each do |greeting|
+    puts "#{greet_characters.capitalize}"
+  end
+  
+  
+  
+  
+  
+
+
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
